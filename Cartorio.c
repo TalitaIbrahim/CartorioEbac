df64b99d5b915b5e8f266d6a2673602b5c83bbcc
TalitaIbrahim <142520570+TalitaIbrahim@users.noreply.github.com>
2023-08-17 12:31:54 -0300

    Alteração no menu - Opção de Saida
    Incluso a opção de sair do sistema no menu inicial para o usuário.

--- Cartorio.c
+++ Cartorio.c
@@ -126,6 +126,7 @@ int main ()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar os nomes\n"); 
 		printf("\t3 - Deletar os nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); // fim do menu
 		
 		scanf("%d", &opcao); // armazenando a escolha do usuário
@@ -144,6 +145,11 @@ int main ()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigada por utilizar nosso sistema!\n");
+		return 0;
 		break;
 		
 		default:
